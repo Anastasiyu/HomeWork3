09ca44c9b819f67922a135a8f83c76de0e85de08
Anastasiyu <asy.safiulina@mail.ru>
2022-08-24 09:18:36 +0300

    Первый комит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public classMain {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
     }
 }
