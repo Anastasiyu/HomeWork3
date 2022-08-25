9044fd2557233818b5b11682707911a3e6482660
Anastasiyu <asy.safiulina@mail.ru>
2022-08-25 16:42:40 +0300

    ДЗ третья задача

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
         int year = 2021;
 
         if (year / 4 == 0 && year / 100 != 0 || year / 400 == 0) {
-     System.out.println(year + "является высокосным");
- } else {
-      System.out.println(year + "не является высокосным");
-}
-}
+            System.out.println(year + "является высокосным");
+        } else {
+            System.out.println(year + "не является высокосным");
+        }
+    }
 }
