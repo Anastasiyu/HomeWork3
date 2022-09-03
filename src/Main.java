58009deecf7f4ab3829363a6adafd5212d9e5417
Anastasiyu <asy.safiulina@mail.ru>
2022-09-03 22:25:12 +0300

    ДЗ вторая задача

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
   // Задание 3
     public static void main(String[] args) {
-
-        int year = 2021;
+        task2();
+    }
+    public static void task1() {
+    int year = 2021;
 
         if (year / 4 == 0 && year / 100 != 0 || year / 400 == 0) {
             System.out.println(year + "является высокосным");
@@ -10,4 +12,24 @@ public class Main {
             System.out.println(year + "не является высокосным");
         }
     }
+    public static void task2() {
+        int clientOS = 0;
+        int clientDeviceYear = 2020;
+        if (clientOS == 0) {
+
+         if (clientDeviceYear < 2015) {
+
+             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+         } else {
+             System.out.println("Установите версию приложения для iOS по ссылке");
+         }
+        } else {
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }
+    }
 }
+
